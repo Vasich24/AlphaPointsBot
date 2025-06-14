c43937170ba049a247591d758853096d0e684538
Vasich24 <4microbe@gmail.com>
2025-06-14 18:39:05 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const API_URL = process.env.API_URL;
 const CHANNEL_ID = '-1001751887491';
 // const CHANNEL_ID = '-1002500459840';
 const CHANNEL_LINK = 'https://t.me/CryptoWayX';
-const SUBS_FILE = path.join(__dirname, 'subs.json');
+const SUBS_FILE = path.join(__dirname, 'subscribed_users.json');
 
 const bot = new Telegraf(BOT_TOKEN);
 bot.use(session());
